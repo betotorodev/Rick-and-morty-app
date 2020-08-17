7ddf8851195907ec250468c7a3431091daa412fb
Beto <betotoro0902@gmail.com>
2020-08-17 18:44:08 -0500

    initial commit

---
+++ src/templates/Header.js
@@ -0,0 +1,19 @@
+const Header = () => {
+  const view = `
+  <div class="header-main">
+    <div class="logo">
+    <h1>
+      <a href="/">
+        RickAndMortyApp
+      </a>
+    </h1>
+    </div>
+    <div class="header-nav">
+      <a href="#/about">About</a>
+    </div>
+  </div>
+  `
+  return view
+}
+
+export const Header
