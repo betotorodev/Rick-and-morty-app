4e961927f4403b734fba448d16a376c2ec387b7a
Beto <betotoro0902@gmail.com>
2020-08-21 08:56:42 -0500

    UPDATE css src

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
       filename: "./index.html"
     }),
     new CopyWebpackPlugin({
-      patterns: [{ from: "./src/styles/styles.css", to: "dist/" }]
+      patterns: [{ from: "./src/styles/styles.css", to: "" }]
     })
   ]
 }
